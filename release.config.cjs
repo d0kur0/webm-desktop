a4167a5cbb5a2f3d9934340cbe117aa02c47a48b
Nkozlov <nkozlov@mtt.ru>
2023-09-29 16:22:36 +0300

    fix: maybe it works?

--- release.config.cjs
+++ release.config.cjs
@@ -7,10 +7,10 @@ module.exports = {
 			"@semantic-release/github",
 			{
 				assets: [
-					{ path: "./dist_windows/squirrel.windows/x64/webm-desktop-${nextRelease.version} Setup.exe" },
-					{ path: "./dist_macos/zip/darwin/x64/webm-desktop-darwin-x64-${nextRelease.version}.zip" },
-					{ path: "./dist_linux/deb/x64/webm-desktop_${nextRelease.version}_amd64.deb" },
-					{ path: "./dist_linux/rpm/x64/webm-desktop-${nextRelease.version}-1.x86_64.rpm" },
+					{ path: "dist_windows/squirrel.windows/x64/webm-desktop-${nextRelease.version} Setup.exe" },
+					{ path: "dist_macos/zip/darwin/x64/webm-desktop-darwin-x64-${nextRelease.version}.zip" },
+					{ path: "dist_linux/deb/x64/webm-desktop_${nextRelease.version}_amd64.deb" },
+					{ path: "dist_linux/rpm/x64/webm-desktop-${nextRelease.version}-1.x86_64.rpm" },
 				],
 			},
 		],
