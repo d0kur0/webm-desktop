14fc518b9bb531635f56adcfc616101219a4190c
Nkozlov <nkozlov@mtt.ru>
2023-09-29 15:41:06 +0300

    feat: add assets glob for release

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,17 @@ module.exports = {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
-		"@semantic-release/github",
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{ path: "./dist_windows/squirrel.windows/x64/**.exe" },
+					{ path: "./dist_macos/zip/darwin/x64/**.zip" },
+					{ path: "./dist_linux/deb/x64/**.deb" },
+					{ path: "./dist_linux/rpm/x64/**.rpm" },
+				],
+			},
+		],
 		[
 			"@semantic-release/changelog",
 			{
@@ -13,15 +23,14 @@ module.exports = {
 		[
 			"@semantic-release/npm",
 			{
-				npmPublish: false
-			}
+				npmPublish: false,
+			},
 		],
 		[
 			"@semantic-release/git",
 			{
 				assets: ["package.json", "CHANGELOG.md"],
-				message:
-					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
 			},
 		],
 	],
