c9ba8fe0349d92be71132d30daa3f169d9a260f7
Nkozlov <nkozlov@mtt.ru>
2023-09-29 18:27:42 +0300

    ci: fix JS syntax err (missing key declaration)

--- release.config.cjs
+++ release.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
 					{ path: "gh_assets/webm-desktop_setup_amd64.exe", label: "Windows Installer" },
 					{ path: "gh_assets/webm-desktop-darwin-x64.zip", label: "MacOS app" },
 					{ path: "gh_assets/webm-desktop_amd64.deb", label: "Installer for Debian-based distro" },
-					{ path: "gh_assets/webm-desktop-1.x86_64.rpm", "Installer for RHat-based distro" },
+					{ path: "gh_assets/webm-desktop-1.x86_64.rpm", label: "Installer for RHat-based distro" },
 				],
 			},
 		],
