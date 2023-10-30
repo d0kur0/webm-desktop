92a842a671f361d354147c3c258802704b77ed4f
d0kur0 <dev.dokuro@gmail.com>
2023-10-30 04:07:51 +0300

    feat: add app icon

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,46 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	packagerConfig: {
+		asar: true,
+		icon: "./icons/icon",
+	},
+	rebuildConfig: {},
+	makers: [
+		{
+			name: "@electron-forge/maker-squirrel",
+			config: {
+				setupIcon: "./icons/icon.ico",
+			},
+		},
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["darwin"],
+			config: {
+				options: {
+					icon: "./icons/icon.icns",
+				},
+			},
+		},
+		{
+			name: "@electron-forge/maker-deb",
+			config: {
+				options: {
+					icon: "./icons/icon.png",
+				},
+			},
+		},
+		{
+			name: "@electron-forge/maker-rpm",
+			config: {
+				options: {
+					icon: "./icons/icon.png",
+				},
+			},
+		},
+	],
+	plugins: [
+		{
+			name: "@electron-forge/plugin-auto-unpack-natives",
+			config: {},
+		},
+	],
 };
