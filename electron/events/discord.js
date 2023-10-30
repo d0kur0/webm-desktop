8ac7bc4012cc931fc23c58699db504f520f3736d
d0kur0 <dev.dokuro@gmail.com>
2023-10-30 04:04:19 +0300

    fix: handle error for setter of discord activity

--- electron/events/discord.js
+++ electron/events/discord.js
@@ -1,26 +1,26 @@
 const { ipcMain } = require("electron");
-const { register, Client } = require('discord-rpc');
+const { register, Client } = require("discord-rpc");
 
-const clientId = '1166773524380274788';
+const clientId = "1166773524380274788";
 register(clientId);
 
-const rpc = new Client({ transport: 'ipc' });
+const rpc = new Client({ transport: "ipc" });
+rpc.login({ clientId }).catch(console.error);
 
-ipcMain.on("discord/changeActivity", (event, fileJson) => {
-	console.log("Activity changed", fileJson);
-	const file = JSON.parse(fileJson);
-
-	rpc.setActivity({
-		details: `/${file.rootThread.board} - ${file.rootThread.subject}`,
-		state: file.name,
-		largeImageKey: file.previewUrl,
-		largeImageText: file.name,
-		smallImageKey: file.previewUrl,
-		smallImageText: file.name,
-		buttons: [
-			{ label: 'Get source', url: file.url }
-		],
+ipcMain.on("discord/changeActivity", (event, file) => {
+	queueMicrotask(async () => {
+		try {
+			await rpc.setActivity({
+				state: file.name,
+				details: `/${file.rootThread.board} - ${file.rootThread.subject}`.slice(0, 127),
+				buttons: [{ label: "Get source", url: file.url }],
+				largeImageKey: file.previewUrl,
+				smallImageKey: file.previewUrl,
+				largeImageText: file.name,
+				smallImageText: file.name,
+			});
+		} catch (e) {
+			// Поебать
+		}
 	});
 });
-
-rpc.login({ clientId }).catch(console.error);
