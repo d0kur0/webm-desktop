693580bd378ff4ba42f7b1212e71447e8f6d9257
d0kur0 <dev.dokuro@gmail.com>
2024-01-26 02:59:15 +0300

    fix: remove discord-rpc and update electron

--- electron/events/discord.js
+++ electron/events/discord.js
@@ -1,30 +1,5 @@
 const { ipcMain } = require("electron");
-const { register, Client } = require("discord-rpc");
-
-try {
-	const clientId = "1166773524380274788";
-	register(clientId);
-
-	const rpc = new Client({ transport: "ipc" });
-	rpc.login({ clientId }).catch(() => {});
-} catch (e) {
-	// Поебать
-}
 
 ipcMain.on("discord/changeActivity", (event, file) => {
-	queueMicrotask(async () => {
-		try {
-			await rpc.setActivity({
-				state: file.name,
-				details: `/${file.rootThread.board} - ${file.rootThread.subject}`.slice(0, 127),
-				buttons: [{ label: "Get source", url: file.url }],
-				largeImageKey: file.previewUrl,
-				smallImageKey: file.previewUrl,
-				largeImageText: file.name,
-				smallImageText: file.name,
-			});
-		} catch (e) {
-			// Поебать
-		}
-	});
+	console.log("not implemented", event, file);
 });
